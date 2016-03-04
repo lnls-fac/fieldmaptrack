08b2e8d88e09f9437df279c8e44123cb2d37d0fc
ximenes <xresende@gmail.com>
2016-03-04 14:51:32 -0300

    Finish fma-multifunctional-sextupole.py

--- scripts/fma-multifunctional-sextupole.py
+++ scripts/fma-multifunctional-sextupole.py
@@ -12,6 +12,9 @@ def read_analysis_file(fname):
         lines = fp.readlines()
     data = {}
     for line in lines:
+        if 'r0_for_relative_multipoles' in line:
+            words = line.split()
+            r0 = float(words[1])
         if line.startswith('n='):
             words = line.split()
             n = int(words[0][2:4])
@@ -23,9 +26,9 @@ def read_analysis_file(fname):
                 integ_skew_multipole = float(words[6])
             except:
                 integ_skew_multipole = 0
-            #print(n,integ_normal_multipole,integ_skew_multipole)
+
             data[n] = (integ_normal_multipole, integ_skew_multipole)
-    return data
+    return data, r0
 
 def get_data(text):
     ok, files = dialog.directories_dialog(path=_default_dir, name=text)
@@ -40,25 +43,42 @@ def get_data(text):
 
     return files[0], read_analysis_file(files[0] + '/analysis.txt')
 
-def print_difference(fname, data0, data1):
+def print_difference(fname, data0, data1, order, mtype, r0):
+
+    t = {'normal':0,'skew':1}[mtype]
+
+    r0 = r0/1000.0
+    mmult = data1[order][t]*(r0**order)
+    m = {}
+    for h in data1.keys():
+        m[h] = ((data1[h][0] - data0[h][0])*(r0**h)/mmult, (data1[h][1] - data0[h][1])*(r0**h)/mmult)
+
     print(fname + ':')
-    header = 'harm: normal0    skew0      | normal1    skew1      | normal_dif skew_dif  '
+    header = 'harm: normal0    skew0      | normal1    skew1      | rel_normal_dif_r0 rel_skew_dif_r0'
     print('-'*(1+len(fname)))
     print(header)
     print('-'*len(header))
     for h in data1.keys():
         n1, s1 = data1[h]
+        n2, s2 = m[h]
         try:
             n0, s0 = data0[h]
         except:
             n0, s0 = 0.0, 0.0
-        print('n={0:02d}: {1:+.3e} {2:+.3e} | {3:+.3e} {4:+.3e} | {5:+.3e} {6:+.3e}'.format(h,n0,s0,n1,s1,n1-n0,s1-s0))
+        print('n={0:02d}: {1:+.3e} {2:+.3e} | {3:+.3e} {4:+.3e} | {5:+.3e} {6:+.3e}'.format(h,n0,s0,n1,s1,n2,s2))
 
 def run():
-    fname, data_sx = get_data(text = 'Select folder containing fieldmap analysis of sextupolar-only excitation')
-    while True:
-        fname, data = get_data(text = 'Select folder containing fieldmap analysis of an additional excitation')
-        print_difference(fname, data_sx, data)
+    fname, data = get_data(text = 'Select folder containing fieldmap analysis of sextupolar-only excitation')
+    data_sx, r0_0 = data
+    fname, data = get_data(text = 'Select folder containing fieldmap analysis of an additional excitation')
+    data_m, r0_1 = data
+    if r0_0 != r0_1: raise Exception('differete values for r0!')
+    ok, order = dialog.input_dialog('please define main multipole order (0:dipoe|1:quadrupole|...)',def_answer='0',name='main multipole order')
+    if not ok: return
+    ok, mtype = dialog.input_dialog('please define type of main multipole (normal|skew)',def_answer='normal',name='main multipole type')
+    if not ok: return
+    print_difference(fname, data_sx, data_m, int(order[0]), mtype[0], r0_0)
 
 if __name__ == "__main__":
+
     run()
