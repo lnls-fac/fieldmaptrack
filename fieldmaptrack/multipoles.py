bfe13b8291fc4ee45770dd881773a16050e0491b
Fernando <fernandohds564@gmail.com>
2020-04-03 09:48:50 -0300

    MNT: Add polyfit method to remove mathphys.functions dependency.

--- fieldmaptrack/multipoles.py
+++ fieldmaptrack/multipoles.py
@@ -213,3 +213,40 @@ class Multipoles:
                     for j in range(len(self.normal_field_fitting_monomials)):
                         fp.write('{0:+.16e} '.format(self.normal_multipoles[j, i]))
                 fp.write('\n')
+
+    @staticmethod
+    def polyfit(x, y, monomials, algorithm='lstsq'):
+        """Implement Custom polyfit."""
+        X = _np.zeros((len(x), len(monomials)))
+        N = _np.zeros((len(x), len(monomials)))
+        for i in range(X.shape[1]):
+            X[:, i] = x
+            N[:, i] = monomials[i]
+        XN = X ** N
+        y_ = _np.zeros((len(y), 1))
+        y_[:, 0] = y
+        XNt = _np.transpose(XN)
+        b = _np.dot(XNt, y_)
+        X = _np.dot(XNt, XN)
+
+        # TODO: make algorithm == 'numpy' the only option of this method.
+        # need to test with real fieldmap analysis.
+        if algorithm is 'lstsq':
+            r = _np.linalg.lstsq(X, b)
+            coeffs = r[0][:, 0]
+        elif algorithm is 'numpy':
+            coeffs = _np.polynomial.polynomial.polyfit(x, y, deg=monomials)
+            coeffs = coeffs[monomials]
+        else:
+            r = _np.linalg.solve(X, b)
+            coeffs = r[:, 0]
+
+        # TODO: vectorize this code! Use numpy methods.
+        # finds maximum diff and its base value
+        y_fitted = _np.dot(XN, coeffs)
+        y_diff = abs(y_fitted - y_[:, 0])
+        max_error = max(y_diff)
+        idx = [i for i, value in enumerate(y_diff) if value == max_error]
+        base_value = y_[idx[0], 0]
+
+        return (coeffs, (max_error, base_value))
